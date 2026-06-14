ed531f38e21afa6a2f7f460d190cd5eae9f5e02d
HiguitaR <devjuan06@gmail.com>
2026-06-14 09:56:07 -0400

    Bugs fixed, about database connections

--- medicare/src/main/java/com/higuitar/medicare/repository/jpa/UserRepository.java
+++ medicare/src/main/java/com/higuitar/medicare/repository/jpa/UserRepository.java
@@ -1,7 +1,7 @@
 package com.higuitar.medicare.repository.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
+import com.higuitar.medicare.model.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 }
